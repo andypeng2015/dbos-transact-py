f8e52c3e9fd515f1b983e83dfc12ee769ba8bdab
Harry Pierson <harry.pierson@dbos.dev>
2024-09-20 09:58:32 -0700

    DRY _start_workflow

--- dbos/core.py
+++ dbos/core.py
@@ -415,27 +415,17 @@ def _start_workflow(
     if not execute_workflow:
         return _WorkflowHandlePolling(new_wf_id, dbos)
 
-    if fself is not None:
-        future = dbos._executor.submit(
-            cast(Callable[..., R], _execute_workflow_wthread),
-            dbos,
-            status,
-            func,
-            new_wf_ctx,
-            fself,
-            *args,
-            **kwargs,
-        )
-    else:
-        future = dbos._executor.submit(
-            cast(Callable[..., R], _execute_workflow_wthread),
-            dbos,
-            status,
-            func,
-            new_wf_ctx,
-            *args,
-            **kwargs,
-        )
+    dbos_args = (
+        (dbos, status, func, new_wf_ctx)
+        if fself is None
+        else (dbos, status, func, new_wf_ctx, fself)
+    )
+
+    future = dbos._executor.submit(
+        cast(Callable[..., R], _execute_workflow_wthread),
+        *(dbos_args + args),
+        **kwargs,
+    )
     return _WorkflowHandleFuture(new_wf_id, future, dbos)
 
 
